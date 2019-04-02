e601c0e6aaedb36e973403ce775bcd2b4acd0169
zhanghuawei <vhuawei@sina.com>
2019-04-02 08:10:11 +0800

    update

--- src/router.js
+++ src/router.js
@@ -19,6 +19,10 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/login",
+      component: () => import("@/views/login/login.vue")
     }
   ]
 });
---
+++ src/views/login/login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login">
+    <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+      <el-form-item label="用户名">
+        <el-input v-model="formLabelAlign.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formLabelAlign.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">登陆</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      labelPosition: "right",
+      formLabelAlign: {}
+    };
+  }
+};
+</script>
+<style lang="scss">
+.login {
+  width: 300px;
+  margin: 10% auto;
+  .el-button {
+    width: 100%;
+  }
+}
+</style>
