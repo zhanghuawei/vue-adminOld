4ef8ec78a23f1cb87108952363c18ba3266589d9
zhanghuawei <vhuawei@sina.com>
2019-06-25 10:51:28 +0800

    update

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,7 +6,8 @@ import { getToken } from '@/utils/auth'
 // create an axios instance
 const service = axios.create({
 //   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  baseURL: 'http://rap2api.taobao.org/app/mock/162114',
+  // baseURL: 'http://rap2api.taobao.org/app/mock/162114',
+  baseURL: 'https://xhuawei.com',
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
